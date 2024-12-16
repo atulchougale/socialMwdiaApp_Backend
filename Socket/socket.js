45d41bf8c64f08aaf60ecf75f57620828e20442f
atulchougale <atulchougale96@gmail.com>
2024-12-16 23:44:57 +0530

    io

--- Socket/socket.js
+++ Socket/socket.js
@@ -1,34 +1,42 @@
-const { model } =require ('mongoose');
-
-const { Server } = require ( 'socket.io');
-const http = require ( 'http');
-const express = require ( 'express');
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 
+const server = http.createServer(app);
 
-const server = http.createServer(app)
-const io = new Server(server,{
-    cors:{
-        origin:['http://localhost:3000'],
-        methods:["GET","POST"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:3000', 'https://mysocialapp-07.netlify.app'], // Updated to include both development and production origins
+    methods: ['GET', 'POST'],
+  },
 });
 
- const getReceiverSocketId = (reciverId)=>{
-    return userSocketmap[reciverId];
+// User socket mapping for tracking online users
+const userSocketmap = {};
+
+// Get the socket ID of a receiver by their user ID
+const getReceiverSocketId = (receiverId) => {
+  return userSocketmap[receiverId];
+};
+
+// Handle Socket.IO connections
+io.on('connection', (socket) => {
+  const userId = socket.handshake.query.userId;
+
+  if (userId !== 'undefined') {
+    userSocketmap[userId] = socket.id; // Map userId to socket ID
   }
-  const userSocketmap = {}; 
-  io.on('connection', (socket) => {
-    const userId = socket.handshake.query.userId
-   
-    if(userId !== "undefined") userSocketmap[userId] = socket.id;
-    io.emit("getOnlineUsers",Object.keys(userSocketmap))
-  
-    socket.on('disconnect', () => {
-      delete userSocketmap[userId],   
-    io.emit("getOnlineUsers",Object.keys(userSocketmap))
-    });
+
+  // Emit the updated list of online users
+  io.emit('getOnlineUsers', Object.keys(userSocketmap));
+
+  // Handle user disconnect
+  socket.on('disconnect', () => {
+    delete userSocketmap[userId]; // Remove user from the map
+    io.emit('getOnlineUsers', Object.keys(userSocketmap)); // Broadcast updated list
   });
-  
-  module.exports = {server, app , io , getReceiverSocketId}
+});
+
+module.exports = { server, app, io, getReceiverSocketId };
