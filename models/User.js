216287315fb94c2bcb57449ed9093334ff8a2cde
atulchougale <atulchougale96@gmail.com>
2025-07-30 15:20:44 +0530

    new_30072025

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
       index: true,
-      trim: true,
+      trim: true ,
     },
     email: {
       type: String,
